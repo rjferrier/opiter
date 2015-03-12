df552df65cee6a415b361f0da3142b22059f2363
Richard Ferrier <r.ferrier@imperial.ac.uk>
2015-03-12 15:51:48 +0000

    Tweak DynamicDict and finish its unit tests.

--- src/dynamic_dict.py
+++ src/dynamic_dict.py
@@ -1,24 +1,29 @@
+from types import FunctionType 
 
 class DynamicDict(dict):
-    """This works the same way as a conventional dict, but it allows
-    values to be runtime-dependent upon the state of others.  Each of
-    these values is a DynamicEntry."""
+    """A DynamicDict works the same way as a conventional dict, but it
+    allows values to be runtime-dependent upon the state of other values
+    in the dict.  Each of these special values is specified as a
+    function accepting a single dictionary argument.  The dictionary is
+    used within the function body to look things up dynamically.
+
+    Because the DynamicDict works by converting all function objects
+    recursively, it is not able to return other functions specified by
+    the client unless these are wrapped as callable objects.
+    """
     def __getitem__(self, key):
         value = dict.__getitem__(self, key)
-        # recurse until the return value is no longer a lambda function
-        if isinstance(value, DynamicEntry):
+        # recurse until the return value is no longer a function
+        if isinstance(value, FunctionType):
             return value(self)
         else:
             return value
-
-class DynamicEntry:
-    def __init__(self, function):
-        """Returns a value which is runtime-dependendent on other values
-        in a dictionary.  The object is initialised with a lambda
-        function accepting a single dictionary argument.  The dictionary
-        may be used within the function body to look up stuff."""
-        self.function = function
-    def __call__(self, dictionary):
-        return self.function(dictionary)
-
         
+class CallableEntry:
+    """Can be used in a DynamicDict to wrap a function."""
+    def __init__(self, function):
+        self.function = function
+    def __call__(self, *args, **kwargs):
+        return self.function(*args, **kwargs)
+    
+    
--- test/test_dynamic_dict.py
+++ test/test_dynamic_dict.py
@@ -7,35 +7,63 @@ from dynamic_dict import *
 class TestDynamicDict(unittest.TestCase):
     
     def setUp(self):
-        # create DynamicDict with a lambda-based DynamicEntry
-        d = DynamicDict({
-                'fahrenheit': DynamicEntry(
-                    lambda d: d['celsius']*9./5 + 32.),
+        # create DynamicDict with a lambda-based entry
+        self.dynamic_dict = DynamicDict({
+                'fahrenheit': lambda d: d['celsius']*9./5 + 32.,
                 })
-        # also create a DynamicEntry using a conventional function
-        def sensation_function(d):
-            "Helper function"
-            if d['celsius'] <= 20.:
-                return 'cold'
-            elif d['celsius'] <= 50.:
-                return 'warm'
+        # add a conventionally defined function
+        def water_state(d):
+            if d['celsius'] < 0.:
+                return 'ice'
+            elif d['celsius'] < 100.:
+                return 'liquid'
             else:
-                return 'hot'
-        # and use it to update the DynamicDict
-        d['sensation']: DynamicEntry(sensation_function)
-        
+                return 'steam'
+        self.dynamic_dict['water_state'] = water_state
 
     def test_missing_information_raises_error(self):
-        pass                    # tk
+        self.assertRaises(KeyError, 
+                          lambda: self.dynamic_dict['fahrenheit'])
 
     def test_lambda_based_entry(self):
-        d['celsius'] = 0.
-        self.assertAlmostEqual(d['fahrenheit'], 32.)
-        d['celsius'] = 100.
-        self.assertAlmostEqual(d['fahrenheit'], 212.)
+        self.dynamic_dict['celsius'] = 0.
+        self.assertAlmostEqual(
+            self.dynamic_dict['fahrenheit'], 32.)
+        self.dynamic_dict['celsius'] = 100.
+        self.assertAlmostEqual(
+            self.dynamic_dict['fahrenheit'], 212.)
 
     def test_def_based_entry(self):
-        pass                    # tk
+        self.dynamic_dict['celsius'] = -10.
+        self.assertEqual(
+            self.dynamic_dict['water_state'], 'ice')
+        self.dynamic_dict['celsius'] = 50.
+        self.assertAlmostEqual(
+            self.dynamic_dict['water_state'], 'liquid')
+
+    def test_nested_entry(self):
+        # modify the dict
+        def human_response(d):
+            if d['water_state']=='steam':
+                return 'ouch!'
+        self.dynamic_dict['human_response'] = human_response
+        # test for different states
+        self.dynamic_dict['celsius'] = 0.
+        self.assertIsNone(self.dynamic_dict['human_response'])
+        self.dynamic_dict['celsius'] = 120.
+        self.assertEqual(self.dynamic_dict['human_response'], 'ouch!')
+
+    def test_custom_function_entry(self):
+        # need to wrap custom functions in a callable class
+        def my_function(a, b, scale_b=1.):
+            return a + b*scale_b
+        self.dynamic_dict['my_function'] = CallableEntry(my_function)
+        # test 
+        self.assertAlmostEqual(
+            self.dynamic_dict['my_function'](2., 3.), 5.)
+        self.assertAlmostEqual(
+            self.dynamic_dict['my_function'](2., 3., scale_b=2.), 8.)
+
+if __name__ == '__main__':
+    unittest.main()
         
-
-
