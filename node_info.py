580c68f29545fc71e9dd9322492b582c0b558d74
Richard Ferrier <r.ferrier@imperial.ac.uk>
2015-05-27 17:44:58 +0100

    Modify TreeFormatter to work correctly with anonymous (blank) nodes.

--- node_info.py
+++ node_info.py
@@ -192,6 +192,12 @@ class ArrayNodeInfo(NodeInfo):
 
 
 class SimpleFormatter:
+    """
+    Function object that represents a combination of nodes as
+    c0:n0_c1:n1_c2:n2, where ci and ni are the collection and node
+    names associated with the i'th node.  The ':' and '_' separators
+    can be defined upon initialisation.
+    """
     def __init__(self, node_separator='_', collection_separator=None):
         self.node_separator = node_separator
         self.collection_separator = collection_separator
@@ -212,28 +218,48 @@ class SimpleFormatter:
 
         
 class TreeFormatter:
+    """
+    Function object that represents a combination of nodes as a tree
+    fragment, e.g.
+    c0:n0
+        c1:n1
+            c2:n2
+    where ci and ni are the collection and node names associated with
+    the i'th node.  The indent and the ':' separator can be defined
+    upon initialisation.
+
+    When the object is made to operate on a complete list of node
+    combinations in serial, the complete tree emerges.
+    """
     def __init__(self, indent_string=' '*4, collection_separator=': '):
         self.indent_string = indent_string
         self.collection_separator = collection_separator
-
+ 
     def __call__(self, node_info_list, absolute={}, relative={}):
-        stem = ''
         branch = ''
+        indent = ''
         for level, ni in enumerate(node_info_list):
-            if ni.is_first():
-                # keep adding branch descriptors to the stem string if
-                # this is the first node in an array
-                if branch:
-                    stem += branch + '\n'
-                else:
-                    stem += branch
-            else:
-                # if not the first node, the stem up to this point
-                # will have already been printed.  So reset it here
-                stem = ''
-            # build a branch descriptor
-            branch = level*self.indent_string + \
-                     ni.str(absolute=absolute, relative=relative, 
+            # get a descriptor for the current node
+            substr = ni.str(absolute=absolute, relative=relative, 
                             collection_separator=self.collection_separator)
-        # the end branch is basically a leaf and is always printed
-        return stem + branch
+
+            # if not the first node, the branch up to this point will
+            # have already been printed, so reset the branch
+            if not ni.is_first():
+                branch = ''
+
+            # cycle if blank
+            if not substr:
+                continue
+
+            # if we are not at the root, do a newline in preparation
+            # for the new node
+            if branch:
+                branch += '\n'
+
+            # add the node to the branch and increase the indent for
+            # next time
+            branch += indent + substr
+            indent += self.indent_string
+
+        return branch
--- test/test_unit_formatters.py
+++ test/test_unit_formatters.py
@@ -11,30 +11,35 @@ class FakeNodeInfo:
     For representing a node on this tree:
     0: a
         1: a
-            2: a
-            2: b
-            2: c
-        1: b
-            2: a
-            2: b
-            2: c
+            2: a ...
     """
-    def __init__(self, level, node_name):
+    ref_nodes = list('abcd')
+
+    def __init__(self, level, node_name, node_index):
         self.level = level
         self.node_name = node_name
+        self.node_index = node_index
 
     @classmethod
-    def combo(Self, node_names):
-        return [Self(i, node_name) for i, node_name in enumerate(node_names)]
+    def combo(Self, node_names, anon=None):
+        result = []
+        for i, node_name in enumerate(node_names):
+            node_index = Self.ref_nodes.index(node_name)
+            if i == anon:
+                node_name = None
+            result.append(Self(i, node_name, node_index))
+        return result
 
     def str(self, absolute=None, relative=None, collection_separator=None):
+        if not self.node_name:
+            return ''
         result = ''
         if collection_separator:
             result += str(self.level) + collection_separator
         return result + self.node_name
 
     def is_first(self):
-        return self.node_name == 'a'
+        return self.node_index == 0
 
         
 class TestSimpleFormatter(unittest.TestCase):
@@ -46,8 +51,8 @@ class TestSimpleFormatter(unittest.TestCase):
         self.node_info = FakeNodeInfo.combo('abc')
         self.assertEqual(self.formatter(self.node_info), 'a_b_c')
 
-    def test_simple_formatter_with_blank(self):
-        self.node_info = FakeNodeInfo.combo(['a', '', 'c'])
+    def test_simple_formatter_with_anonymous_middle_node(self):
+        self.node_info = FakeNodeInfo.combo('abc', anon=1)
         self.assertEqual(self.formatter(self.node_info), 'a_c')
 
     def test_simple_formatter_with_custom_separator(self):
@@ -55,23 +60,47 @@ class TestSimpleFormatter(unittest.TestCase):
         self.formatter = SimpleFormatter(',')
         self.assertEqual(self.formatter(self.node_info), 'a,b,c')
 
-    
+
 class TestTreeFormatter(unittest.TestCase):
 
+    def make_tree(self, node_combos, anon=None, formatter=TreeFormatter()):
+        result = ''
+        for c in node_combos:
+            branch = formatter(FakeNodeInfo.combo(c, anon))
+            if branch:
+                result += '\n' + branch
+        return result
+        
+
+        
+class TestTreeFormatterWithThreeLevels(TestTreeFormatter):
+
     def setUp(self):
-        self.formatter = TreeFormatter()
+        """
+        Run tests on this tree:
+        0: a
+            1: a
+                2: a
+                2: b
+                2: c
+            1: b
+                2: a
+                2: b
+                2: c
+        """
+        self.node_combos = product('a', 'ab', 'abc')
+
 
     def test_tree_formatter_first_combo(self):
         """
         I want to print part of the tree associated with the very first
         node combination, [a,a,a].
         """
-        node_info = FakeNodeInfo.combo('aaa')
         expected = """
 0: a
     1: a
         2: a"""
-        self.assertEqual('\n'+self.formatter(node_info), expected)
+        self.assertEqual(self.make_tree(['aaa']), expected)
 
 
     def test_tree_formatter_intermediate_combo(self):
@@ -79,11 +108,10 @@ class TestTreeFormatter(unittest.TestCase):
         I want to print part of the tree associated with an intermediate
         combination, [a,b,a].
         """
-        node_info = FakeNodeInfo.combo('aba')
         expected = """
     1: b
         2: a"""
-        self.assertEqual('\n'+self.formatter(node_info), expected)
+        self.assertEqual(self.make_tree(['aba']), expected)
 
 
     def test_tree_formatter_leaf_combo(self):
@@ -91,17 +119,48 @@ class TestTreeFormatter(unittest.TestCase):
         I want to print e.g. [a,a,b] which does not have any preamble when
         it is printed.
         """
-        node_info = FakeNodeInfo.combo('aab')
         expected = """
         2: b"""
-        self.assertEqual('\n'+self.formatter(node_info), expected)
+        self.assertEqual(self.make_tree(['aab']), expected)
 
 
+    def test_tree_formatter_intermediate_combo_with_custom_separator(self):
+        formatter = TreeFormatter(collection_separator='-')
+        expected = """
+    1-b
+        2-a"""
+        self.assertEqual(self.make_tree(['aba'], formatter=formatter),
+                         expected)
+
+
+    def test_tree_formatter_intermediate_combo_omitting_collection_names(self):
+        """
+        I want to omit the collection names altogether, so I'll specify
+        the collection separator as None.
+        """
+        formatter = TreeFormatter(collection_separator=None)
+        expected = """
+    b
+        a"""
+        self.assertEqual(self.make_tree(['aba'], formatter=formatter),
+                         expected)
+
+
+    def test_tree_formatter_intermediate_combo_with_custom_indent(self):
+        formatter = TreeFormatter(indent_string='...')
+        expected = """
+...1: b
+......2: a"""
+        self.assertEqual(self.make_tree(['aba'], formatter=formatter),
+                         expected)
+        
+        
     def test_tree_formatter_all(self):
         """
         I want to print the entire tree.
         """
-        expected = """0: a
+        expected = """
+0: a
     1: a
         2: a
         2: b
@@ -110,42 +169,91 @@ class TestTreeFormatter(unittest.TestCase):
         2: a
         2: b
         2: c"""
-        results = [self.formatter(FakeNodeInfo.combo(c)) \
-                   for c in product('a', 'ab', 'abc')]
-        self.assertEqual('\n'.join(results), expected)
+        self.assertEqual(self.make_tree(self.node_combos), expected)
 
 
-    def test_tree_formatter_intermediate_combo_with_custom_separator(self):
-        self.formatter = TreeFormatter(collection_separator='-')
-        node_info = FakeNodeInfo.combo('aba')
+    def test_tree_formatter_all_with_anonymous_first_node(self):
         expected = """
-    1-b
-        2-a"""
-        self.assertEqual('\n'+self.formatter(node_info), expected)
+1: a
+    2: a
+    2: b
+    2: c
+1: b
+    2: a
+    2: b
+    2: c"""
+        self.assertEqual(self.make_tree(self.node_combos, anon=0), expected)
 
 
-    def test_tree_formatter_intermediate_combo_omitting_collection_names(self):
-        """
-        I want to omit the collection names altogether, so I'll specify
-        the collection separator as None.
-        """
-        self.formatter = TreeFormatter(collection_separator=None)
-        node_info = FakeNodeInfo.combo('aba')
+    def test_tree_formatter_all_with_anonymous_intermediate_node(self):
         expected = """
-    b
-        a"""
-        self.assertEqual('\n'+self.formatter(node_info), expected)
-
-
-    def test_tree_formatter_intermediate_combo_with_custom_indent(self):
-        self.formatter = TreeFormatter(indent_string='...')
-        node_info = FakeNodeInfo.combo('aba')
-        expected = """
-...1: b
-......2: a"""
-        self.assertEqual('\n'+self.formatter(node_info), expected)
+0: a
+    2: a
+    2: b
+    2: c
+    2: a
+    2: b
+    2: c"""
+        self.assertEqual(self.make_tree(self.node_combos, anon=1),
+                         expected)
 
         
+    def test_tree_formatter_all_with_anonymous_end_node(self):
+        expected = """
+0: a
+    1: a
+    1: b"""
+        self.assertEqual(self.make_tree(self.node_combos, anon=2),
+                         expected)
+
+        
+class TestTreeFormatterWithFourLevels(TestTreeFormatter):
+
+    def setUp(self):
+        """
+        Run tests on this tree:
+        0: a
+            1: a
+                2: a
+                    3: a
+                2: b
+                    3: a
+            1: b
+                2: a
+                    3: a
+                2: b
+                    3: a
+        """
+        self.node_combos = product('a', 'ab', 'ab', 'a')
+        
+
+    def test_tree_formatter_four_levels_with_anonymous_second_node(self):
+        expected = """
+0: a
+    2: a
+        3: a
+    2: b
+        3: a
+    2: a
+        3: a
+    2: b
+        3: a"""
+        results = self.make_tree(self.node_combos, anon=1)
+        self.assertEqual(results, expected)
+        
+        
+    def test_tree_formatter_four_levels_with_anonymous_third_node(self):
+        expected = """
+0: a
+    1: a
+        3: a
+        3: a
+    1: b
+        3: a
+        3: a"""
+        results = self.make_tree(self.node_combos, anon=2)
+        self.assertEqual(results, expected)
+
         
 class TestEmptyNodeInfo(unittest.TestCase):
 
@@ -154,3 +262,8 @@ class TestEmptyNodeInfo(unittest.TestCase):
 
     def test_tree_formatter(self):
         self.assertEqual(TreeFormatter()([]), '')
+
+
+if __name__ == '__main__':
+    unittest.main()
+        
